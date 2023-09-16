b3ecd987b2bf769e741d733b90191a932f459a05
Riley Kinahan <kinahanriley@gmail.com>
2023-09-16 15:31:01 -0600

    ASP route

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS;    # allows interacting with other servers
 
+from pymongo.mongo_client import MongoClient
+
 # app instance
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +14,35 @@ def return_home():
         "message": "Hello World!!!",
         "people": ["Sheikh", "Riley", "Peter", "Andy"]
     })
+
+@app.route("/api/home/models")
+def solve_graph():
+    import clingo
+    with open('server/asp program/program.lp', 'r') as file:
+        program = file.read()
+    with open('server/asp program/colors.lp', 'r') as file:
+        colors = file.read()
+    with open('server/asp program/graph.lp', 'r') as file:
+        graph = file.read()
+
+    ctl = clingo.Control()
+    ctl.add("pro", [], program + colors + graph)
+    ctl.ground([("pro",[])])
+    ctl.configuration.solve.models="1"      # max number of models to calculate, 0 for all
+    models = []
+    with ctl.solve(yield_=True) as handle:
+        for model in handle:
+            # select all atoms which would be shown in program output
+            models.append(model.symbols(shown=True))
+    model = models[0]
+    list_models = list()
+    graph = dict()
+    for atom in model:
+        vertex = str(atom.arguments[0])
+        color = str(atom.arguments[1])
+        graph[vertex] = color
+
+    return(jsonify(graph))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
