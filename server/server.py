5c57270c27a7bf60edd914785baa4f6cfcca2dce
SheikhSafwan <sakifhossain71@gmail.com>
2023-09-16 19:25:11 -0600

    Revert "Database created"
    This reverts commit 287f5f93969ad36bd934096750942d79913003a2.

--- server/server.py
+++ server/server.py
@@ -1,32 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS;    # allows interacting with other servers
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-from pymongo.mongo_client import MongoClient
-
-uri = os.getenv("URI")
-# Create a new client and connect to the server
-client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-db = client['GraphDB']
-collection = db['Nodes']
-
-for document in collection.find():
-    print(document)
-
-client.close()
-
-
 
 # app instance
 app = Flask(__name__)
