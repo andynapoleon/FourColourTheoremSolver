20483bfb56c5264b29ba2582e94a71ef088a422e
Andy Tran <anhquoctran006@gmail.com>
2024-07-13 19:52:04 -0600

    cleaning up

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,7 +6,7 @@ import ConditionalNavBar from "./components/ConditionalNavBar";
 const inter = Anek_Gurmukhi({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Four-Colour Theorem Map Coloring",
+  title: "Four-Colour Theorem Map Coloring - Great App!",
   description:
     "A solver application for the famous Four-Colour Map Theorem. Initially built collaboratively during DevelopEd 2.0 Hackathon (2023) and later further developed.",
 };
